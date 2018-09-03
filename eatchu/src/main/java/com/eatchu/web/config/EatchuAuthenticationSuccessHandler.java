314ee3ef4d57274d824f5787e95910191fdf88a8
sora <sorastar31@gmail.com>
2018-09-03 15:36:23 +0900

    ㅋㅋㅋㅋ엄한데 만든클래스 다시 가져옴

---
+++ eatchu/src/main/java/com/eatchu/web/config/EatchuAuthenticationSuccessHandler.java
@@ -0,0 +1,21 @@
+package com.eatchu.web.config;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EatchuAuthenticationSuccessHandler implements AuthenticationSuccessHandler{
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		
+	}
+}
